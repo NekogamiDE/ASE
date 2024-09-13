71823c4728ec6637d1af839400c61517724c52d9
NekogamiDE <hutertimo9.8@gmail.com>
2024-09-13 21:23:55 +0200

    Neu

---
+++ Klassen/TR.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogReader
+{
+    internal class TR
+    {
+        private int team;
+        private int win;
+
+        public TR(int team)
+        {
+            this.team = team;
+            this.win = 0;
+        }
+        public void AddWin(int win)
+        {
+            this.win += win;
+        }
+        public int GetTeam()
+        {
+            return this.team;
+        }
+        public int GetWin()
+        {
+            return this.win;
+        }
+    }
+}
